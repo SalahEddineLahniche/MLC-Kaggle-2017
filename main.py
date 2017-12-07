0da8baa204505a1b24d19fcf27d4a6c863ed9d2b
Salah Eddine <salah.lahniche@finamaze.com>
2017-12-07 20:27:25 +0100

    add csv regexp

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 """Authors: Salah&Yassir"""
+import re
 
 TRAIN_PATH = 'data/train.csv'
 TEST_PATH = 'data/test.csv'
+
+CSV_REGEXP = re.compile(r"(?<=^|,)(\"(?:[^\"]|\"\")*\"|[^,]*)")
 
 def read_n_lines(n: int, new_file: str):
     with open(TRAIN_PATH) as f:
         with open(new_file, "w") as g:
             for i in range(n):
                 g.write(next(f))
+
+
 
 if __name__ == '__main__':
     read_n_lines(100, 'data/truncated_training_dataset.csv')
