335dfeb33dfc0d0150a30bcf67baba674d3b2e93
Salah Eddine <salah.lahniche@finamaze.com>
2017-12-07 20:01:15 +0100

    adding function to trancate the data

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 """Authors: Salah&Yassir"""
 
-print('hello world !')
+TRAIN_PATH = 'data/train.csv'
+TEST_PATH = 'data/test.csv'
+
+def read_n_lines(n: int, new_file: str):
+    with open(TRAIN_PATH) as f:
+        with open(new_file, "w") as g:
+            for i in range(n):
+                g.write(next(f))
+
+if __name__ == '__main__':
+    read_n_lines(100, 'data/truncated_training_dataset.csv')
