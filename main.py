6a2ffce6eeb787a89076bced12bcf20d6fe45145
Yassir <ya.hassini@gmail.com>
2017-12-07 20:26:56 +0100

    adding some exploratory code

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 """Authors: Salah&Yassir"""
+import numpy as np
+import pandas as pd
 
 TRAIN_PATH = 'data/train.csv'
 TEST_PATH = 'data/test.csv'
+COLUMNS_INDEXES = {
+    'eeg': 1,
+    'night': 9,
+    'number_previous': 6,
+    'power_increase': 10,
+    'respiration_x': 2,
+    'respiration_y': 3,
+    'respiration_z': 4,
+    'time': 7,
+    'time_previous': 5,
+    'user': 8
+}
 
 def read_n_lines(n: int, new_file: str):
     with open(TRAIN_PATH) as f:
@@ -9,5 +23,13 @@ def read_n_lines(n: int, new_file: str):
             for i in range(n):
                 g.write(next(f))
 
+f=open('data/truncated_training_dataset.csv')
+columns=next (f).split(',')[1:]
+line1=next(f).split(',')
+
+df = pd.DataFrame(line1, index=columns)
+print(df.head())
+
 if __name__ == '__main__':
     read_n_lines(100, 'data/truncated_training_dataset.csv')
+
